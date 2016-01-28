233568a891c74b4a5ab44d92b53df799f19ead34
nimo-mo <nimo.mo@wolaidai.com>
2016-01-28 15:59:35 +0800

    add api

--- src/scripts/js/api.js
+++ src/scripts/js/api.js
@@ -1,5 +1,36 @@
 var api = {};
-api.request = function (argument) {
-	// body...
+
+api.request = function (type,url,data,ajaxSettings) {
+	return $.ajax($.extend({
+		url: url,
+		type: type,
+		data: JSON.stringify(data),
+		headers: {
+			'X-User-Id': $.cookie('X-User-Id'),
+			'X-User-Token': $.cookie('X-User-Token')
+		}
+	},ajaxSettings||{}))
 };
+
+api.get = function (url,data,ajaxSettings) {
+	return api.request('GET',url,data,ajaxSettings)
+};
+
+api.put = function (url,data,ajaxSettings) {
+	return api.request('PUT',url,data,ajaxSettings)
+};
+
+api.post = function (url,data,ajaxSettings) {
+	return api.request('POST',url,data,ajaxSettings)
+};
+
+api.upload = function (url,data,ajaxSettings) {
+	return api.request('POST',url,{},{
+		data: data,
+		cache: false,
+		contentType: false,
+		processData: false
+	})
+};
+
 module.exports = api;
--- src/scripts/jsx/login.jsx
+++ src/scripts/jsx/login.jsx
@@ -1,16 +1,72 @@
 var React = require('react');
+var Api = require('../js/api');
 
 var Login = React.createClass({
 	contextTypes: {
     router: React.PropTypes.object.isRequired
   },
+  getInitialState: function() {
+  	return {
+  		username: '',
+  		password: '',
+  		errorMsg: '',
+  		errorCls: ''
+  	}
+  },
+  setValue: function (e) {
+  	var input = e.target;
+  	if (input.name=='username') {
+  		this.setState({username:input.value});
+  	}
+  	if (input.name=='password') {
+  		this.setState({password:input.value});
+  	}
+  },
+  login: function () {
+  	var username = this.state.username;
+  	var password = this.state.password;
+  	if (username == '') {
+  		this.setState({
+  			errorMsg: '请输入用户名',
+  			errorCls: 'visible'
+  		});
+  		return;
+  	}
+
+  	if (password == '') {
+  		this.setState({
+  			errorMsg: '请输入密码',
+  			errorCls: 'visible'
+  		});
+  		return;
+  	}
+
+  	this.setState({
+			errorMsg: '',
+			errorCls: ''
+		});
+
+  	Api.post('https://www.baidu.com/',{
+			username: username,
+			password: password
+		}).done(function (data, textStatus, jqXHR) {
+		// body...
+  	}).fail(function (jqXHR, textStatus, errorThrown) {
+  		// body...
+  	});
+  },
 	render: function() {
 		// console.log(this.context.router);
 		return (
-			<div className="app-container">
-				<div className="app-body full-size">
+			<div className="app-container without-aside">
+				<div className="app-body">
 					<div className="app-content login animate">
-						login
+						<form className="loign-form">
+							<input id="username" name="username" type="text" onChange={this.setValue} />
+							<input id="password" name="password" type="password" onChange={this.setValue} />
+							<button type="button" onTouchTap={this.login}>登录</button>
+						</form>
+						<div className={"error-msg login-error-msg " + this.state.errorCls}>{this.state.errorMsg}</div>
 					</div>
 				</div>
 			</div>
